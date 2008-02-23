549f626d730702fb9da846febb4e0d563cb4312c
Marat Nepomnyashy <marat@vyatta.com>
2008-02-22 16:28:25 -0800

    Initialize variable and check for invalid args.

--- scripts/vyatta-vpn-op.pl
+++ scripts/vyatta-vpn-op.pl
@@ -4,9 +4,16 @@ use strict;
 use lib "/opt/vyatta/share/perl5/";
 
 use Getopt::Long;
-my $op;
+my $op='';
 GetOptions("op=s" => \$op);
 
+if ($op eq '') {
+	die 'No op specified';
+}
+
+if ($op eq 'clear-vpn-ipsec-process') {
+	system '/usr/sbin/ipsec setup restart';
+}
 if ($op eq 'show-vpn-debug') {
 	system '/usr/sbin/ipsec auto --status';
 }
@@ -14,8 +21,5 @@ if ($op eq 'show-vpn-debug-detail') {
 	system '/usr/sbin/ipsec barf';
 }
 
-if ($op eq 'clear-vpn-ipsec-process') {
-	system '/usr/sbin/ipsec setup restart';
-}
-
+die "Unknown op: $op";
 
